6a0b201753e83727011ed6baada2c1ff92c70a6d
lyf3312 <lyf3312@163.com>
2018-01-10 11:28:09 +0800

    modify repository

--- MyGit/src/com/lyf/test/Test2.java
+++ MyGit/src/com/lyf/test/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		System.out.println("this is test2");
 		System.out.println("this is test2...");
+		System.out.println("this is test2...repository");
 	}
 
 }
